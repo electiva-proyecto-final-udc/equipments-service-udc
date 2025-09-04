9f6d7fa2889ebc1dbb88edc0ac0a03af60e2177c
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2025-09-04 14:13:24 -0500

    ADD audit

--- gestionEquipos/src/main/java/com/udc/gestionEquipos/GestionEquiposApplication.java
+++ gestionEquipos/src/main/java/com/udc/gestionEquipos/GestionEquiposApplication.java
@@ -2,12 +2,13 @@ package com.udc.gestionEquipos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GestionEquiposApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GestionEquiposApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEquiposApplication.class, args);
+    }
 }
+
